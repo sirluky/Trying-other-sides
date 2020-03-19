5bf0afbc5c45e31a7b3f425d2059ef75cfd4d4dc
Lukáš Kovář <lkov@post.cz>
2020-03-19 23:52:49 +0100

    tried prvocisla and faktorial in C, used vscode + gcc compiler

---
+++ day 3 Try C/Readme.md
@@ -0,0 +1,7 @@
+# Trying out C & more, lower level stuff
+
+- tomorrow, try to rewrite with pointers instead arrays, and show progress bar
+- then try to rewrite into C++, Rust, Go, Haskell.. Just to know the differencies and which low level language you like the most
+
+- Maybe sometime we can rewrite this to assembly
+- Try to compile this to WebAssembly with any language, compare speed & size.
---
+++ day 3 Try C/hello.c
@@ -0,0 +1,32 @@
+/* Welcome to the Interactive C Tutorial.
+Start by choosing a chapter and
+write your code in this window. */
+
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+  time_t rawtime;
+  struct tm *timeinfo;
+
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
+  printf("Aktuální čas a datum je ... : %s", asctime(timeinfo));
+
+  int cislo;
+  unsigned long vysledek = 1;
+
+  printf("Zadejte cislo jehož chcete faktorial: ");
+
+  scanf("%d", &cislo);
+
+  for (int i = 1; i <= cislo; i++)
+  {
+    vysledek *= i;
+  }
+
+  printf("Hello, World! %ld \n", vysledek);
+
+  return 0;
+}
